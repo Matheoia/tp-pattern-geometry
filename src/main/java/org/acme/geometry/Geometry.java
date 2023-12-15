8589fefeba5bebf5156ac19348429ed57be484e4
Matheo Marechal <121936719+Matheoia@users.noreply.github.com>
2023-12-15 10:05:15 +0100

    Add files via upload

---
+++ src/main/java/org/acme/geometry/Geometry.java
@@ -0,0 +1,13 @@
+package org.acme.geometry;
+
+public interface Geometry {
+	
+	public String getType();
+	
+	public Boolean isEmpty();
+	
+	public void translate(double dx, double dy);
+	
+	public Geometry clone();
+
+}
