57faac610c23fea7851d65d8b7ad4b6ee28f8830
Matheo Marechal <121936719+Matheoia@users.noreply.github.com>
2023-12-15 10:59:24 +0100

    Add files via upload

---
+++ src/test/java/org/acme/geometry/CoordinateTest.java
@@ -0,0 +1,61 @@
+package org.acme.geometry;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CoordinateTest {
+
+	public static final double EPSILON = 1.0e-15;
+
+	@Test
+    public void testDefaultConstructor() {
+        Coordinate emptyCoordinate = new Coordinate();
+        Assert.assertTrue(emptyCoordinate.isEmpty());
+        Assert.assertEquals(Double.NaN, emptyCoordinate.getX(), EPSILON);
+        Assert.assertEquals(Double.NaN, emptyCoordinate.getY(), EPSILON);
+        Assert.assertEquals("[NaN,NaN]", emptyCoordinate.toString());
+    }
+	
+	@Test
+	public void testConstructorXY(){
+		Coordinate c = new Coordinate(3.0, 4.0);
+        
+		Assert.assertEquals(3.0, c.getX(), EPSILON);
+		Assert.assertEquals(4.0, c.getY(), EPSILON);
+		Assert.assertFalse(c.isEmpty());
+		Assert.assertEquals("[3.0,4.0]", c.toString());
+	}
+	
+	@Test
+    public void testIsEmpty() {
+        Coordinate emptyCoordinate = new Coordinate();
+        Assert.assertTrue(emptyCoordinate.isEmpty());
+
+        Coordinate nonEmptyCoordinate = new Coordinate(1.0, 2.0);
+        Assert.assertFalse(nonEmptyCoordinate.isEmpty());
+    }
+	
+
+	@Test
+    public void testGetX() {
+        Coordinate c = new Coordinate(3.0, 4.0);
+        Assert.assertEquals(3.0, c.getX(), EPSILON);
+    }
+
+    @Test
+    public void testGetY() {
+        Coordinate c = new Coordinate(3.0, 4.0);
+        Assert.assertEquals(4.0, c.getY(), EPSILON);
+    }
+
+    @Test
+    public void testToString() {
+        Coordinate c = new Coordinate(3.0, 4.0);
+        Assert.assertEquals("[3.0,4.0]", c.toString());
+
+        Coordinate emptyCoordinate = new Coordinate();
+        Assert.assertEquals("[NaN,NaN]", emptyCoordinate.toString());
+    }
+	
+
+}
